d6a685b5b9199bfc5fc0bac40b45b7cccc4bd346
David Konsumer <konsumer@jetboystudio.com>
2018-11-26 18:39:16 -0800

    more work on clothes

--- src/App.js
+++ src/App.js
@@ -15,12 +15,22 @@ export default () => {
     base: 'male/human',
     skin: skinPalettes.Light,
     hair: hairPalettes.Default,
-    hair_style: 'bald'
+    hair_style: 'bald',
+    shirt: 'none',
+    back: 'none',
+    legs: 'none'
   })
 
   const onChange = e => {
-    setValues({ ...values, [e.target.name]: e.target.value })
+    const fixes = {}
+    if (e.target.name === 'base') {
+      fixes.shirt = 'none'
+      fixes.legs = 'none'
+    }
+    setValues({ ...values, [e.target.name]: e.target.value, ...fixes })
   }
+
+  const gender = values.base.indexOf('female') === -1 ? 'male' : 'female'
 
   return (
     <Fragment>
@@ -82,6 +92,94 @@ export default () => {
               <Option value='xlongknot' label='Extra Long Knot' />
             </Select>
             {values.hair_style !== 'bald' && <ColorPalette name='hair' options={hairPalettes} value={values.hair} onChange={onChange} /> }
+          </div>
+          <div>
+            <h3>Shirt</h3>
+            <Select name='shirt' label='Style' value={values.shirt} onChange={onChange}>
+              <Option value='none' label='None' />
+              <Option value='shirts/longsleeve/male/white_longsleeve' label='White Longsleeve' />
+              <Option value='shirts/longsleeve/male/brown_longsleeve' label='Brown Longsleeve' />
+              <Option value='shirts/longsleeve/male/brown_longsleeve' label='Brown Longsleeve' />
+              <Option value='shirts/longsleeve/male/maroon_longsleeve' label='Maroon Longsleeve' />
+              <Option value='shirts/longsleeve/male/teal_longsleeve' label='Teal Longsleeve' />
+              <Option value='shirts/sleeveless/female/white_pirate' label='White Pirate' />
+              <Option value='shirts/sleeveless/female/brown_pirate' label='Brown Pirate' />
+              <Option value='shirts/sleeveless/female/maroon_pirate' label='Maroon Pirate' />
+              <Option value='shirts/sleeveless/female/teal_pirate' label='Teal Pirate' />
+              <Option value='shirts/sleeveless/female/white_sleeveless' label='White Sleeveless' />
+              <Option value='shirts/sleeveless/female/brown_sleeveless' label='Brown Sleeveless' />
+              <Option value='shirts/sleeveless/female/maroon_sleeveless' label='Maroon Sleeveless' />
+              <Option value='shirts/sleeveless/female/teal_sleeveless' label='Teal Sleeveless' />
+              <Option value='tunics/female/brown_tunic' label='Brown Tunic' />
+              <Option value='tunics/female/maroon_tunic' label='Maroon Tunic' />
+              <Option value='tunics/female/teal_tunic' label='Teal Tunic' />
+              <Option value='tunics/female/white_tunic' label='White Tunic' />
+              <Option value={`chain/mail_${gender}`} label='Chainmail' />
+              <Option value={`chain/tabard/jacket_${gender}`} label='Chain Tabard Jacket' />
+              <Option value='corset_female/corset_black' label='Black Corset' />
+              <Option value='corset_female/corset_brown' label='Brown Corset' />
+              <Option value='corset_female/corset_red' label='Red Corset' />
+              <Option value='dress_female/blue_vest' label='Dress Blue Vest' />
+              <Option value='dress_female/dress_w_sash_female' label='Dress w/ Sash' />
+              <Option value='dress_female/overskirt' label='Overskirt' />
+              <Option value='dress_female/tightdress_black' label='Tight Dress Black' />
+              <Option value='dress_female/tightdress_lightblue' label='Tight Dress Light Blue' />
+              <Option value='dress_female/tightdress_red' label='Tight Dress Red' />
+              <Option value='dress_female/tightdress_white' label='Tight Dress White' />
+              <Option value='dress_female/underdress' label='Under-dress' />
+              <Option value='robes_female_no_th-sh/black' label='Black Robes' />
+              <Option value='robes_female_no_th-sh/blue' label='Blue Robes' />
+              <Option value='robes_female_no_th-sh/brown' label='Brown Robes' />
+              <Option value='robes_female_no_th-sh/dark brown' label='Dark Brown Robes' />
+              <Option value='robes_female_no_th-sh/dark gray' label='Grey Robes' />
+              <Option value='robes_female_no_th-sh/forest green' label='Forest Green Robes' />
+              <Option value='robes_female_no_th-sh/light gray' label='Light Grey Robes' />
+              <Option value='robes_female_no_th-sh/purple' label='Purple Robes' />
+              <Option value='robes_female_no_th-sh/red' label='Red Robes' />
+              <Option value='robes_female_no_th-sh/white' label='White Robes' />
+            </Select>
+          </div>
+          <div>
+            <h3>Back</h3>
+            <Select name='back' label='Style' value={values.back} onChange={onChange}>
+              <Option value='none' label='None' />
+              <Option value='cape/normal/female/cape_black' label='Black Cape' />
+              <Option value='cape/normal/female/cape_blue' label='Blue Cape' />
+              <Option value='cape/normal/female/cape_brown' label='Brown Cape' />
+              <Option value='cape/normal/female/cape_gray' label='Grey Cape' />
+              <Option value='cape/normal/female/cape_green' label='Green Cape' />
+              <Option value='cape/normal/female/cape_lavender' label='Lavendar Cape' />
+              <Option value='cape/normal/female/cape_maroon' label='Maroon Cape' />
+              <Option value='cape/normal/female/cape_pink' label='Pink Cape' />
+              <Option value='cape/normal/female/cape_red' label='Red Cape' />
+              <Option value='cape/normal/female/cape_white' label='White Cape' />
+              <Option value='cape/normal/female/cape_yellow' label='Yellow Cape' />
+              <Option value='cape/tattered/female/tattercape_black' label='Black Tattered Cape' />
+              <Option value='cape/tattered/female/tattercape_blue' label='Blue Tattered Cape' />
+              <Option value='cape/tattered/female/tattercape_brown' label='Brown Tattered Cape' />
+              <Option value='cape/tattered/female/tattercape_gray' label='Grey Tattered Cape' />
+              <Option value='cape/tattered/female/tattercape_green' label='Green Tattered Cape' />
+              <Option value='cape/tattered/female/tattercape_maroon' label='Maroon Tattered Cape' />
+              <Option value='cape/tattered/female/tattercape_pink' label='Pink Tattered Cape' />
+              <Option value='cape/tattered/female/tattercape_red' label='Red Tattered Cape' />
+              <Option value='cape/tattered/female/tattercape_white' label='White Tattered Cape' />
+              <Option value='cape/tattered/female/tattercape_yellow' label='Yellow Tattered Cape' />
+              <Option value='cape/trimmed/female/trimcape_whiteblue' label='White / Blue Trim Cape' />
+              <Option value='wings/wings_no_th-sh' label='Wings' />
+            </Select>
+          </div>
+          <div>
+            <h3>Legs</h3>
+            <Select name='legs' label='Style' value={values.legs} onChange={onChange}>
+              <Option value='none' label='None' />
+              <Option value={`armor/${gender}/golden_greaves_${gender}`} label='Golden Greaves' />
+              <Option value={`armor/${gender}/metal_pants_${gender}`} label='Metal' />
+              <Option value={`pants/${gender}/magenta_pants_${gender}`} label='Magenta' />
+              <Option value={`pants/${gender}/white_pants_${gender}`} label='White' />
+              <Option value={`pants/${gender}/red_pants_${gender}`} label='Red' />
+              <Option value={`pants/${gender}/teal_pants_${gender}`} label='Teal' />
+              <Option value={`skirt/${gender}/robe_skirt_${gender}`} label='Robe / Skirt' />
+            </Select>
           </div>
         </form>
       </main>
--- src/DisplayCurrent.js
+++ src/DisplayCurrent.js
@@ -8,24 +8,29 @@ import skinPalettes from './palettes/skin.json'
 import hairPalettes from './palettes/hair.json'
 
 // generate a palette-swapping shader from 2 palettes (array of arrays RGB)
-const swapPaletteFrag = (paletteIn, paletteOut) => {
-  const replacements = paletteIn.map((v, i) => [v, paletteOut[i]])
-  console.log({ replacements, paletteIn, paletteOut })
-  return new MultiColorReplaceFilter(replacements, 0.18)
-}
+const swapPaletteFrag = (paletteIn, paletteOut) => new MultiColorReplaceFilter(paletteIn.map((v, i) => [v, paletteOut[i]]), 0.18)
 
 // get array of numeric colors for array of hex-colors
 const colorMap = palette => palette.map(c => chroma(c).num())
 
+// pre-compute replacement palettes
 const lightSkinPalette = colorMap(skinPalettes.Light)
 const defaultHairPalette = colorMap(hairPalettes.Default)
+
+// TODO: generate full spritesheet, or composite-sheets
+// TODO: handle gendered clothes better
+// TODO: handle color-palettes in clothes
+// TODO: figure out basic adjustments of male->female & female->male, and only include one set
 
 export default ({ values, ...props }) => {
   const gender = values.base.indexOf('female') === -1 ? 'male' : 'female'
   const body = PIXI.Texture.fromImage(require(`./images/body/${values.base}.png`))
   const hair = values.hair_style !== 'bald' && PIXI.Texture.fromImage(require(`./images/hair/${gender}/${values.hair_style}.png`))
+  const shirt = values.shirt !== 'none' && PIXI.Texture.fromImage(require(`./images/torso/${values.shirt}.png`))
+  const back = values.back !== 'none' && PIXI.Texture.fromImage(require(`./images/torso/back/${values.back}.png`))
+  const legs = values.legs !== 'none' && PIXI.Texture.fromImage(require(`./images/legs/${values.legs}.png`))
   const bodyShader = values.base !== 'male/skeleton' && swapPaletteFrag(lightSkinPalette, colorMap(values.skin))
-  const hairShader = swapPaletteFrag(defaultHairPalette, colorMap(values.hair))
+  const hairShader = hair && swapPaletteFrag(defaultHairPalette, colorMap(values.hair))
   return (
     <Stage {...props} options={{ backgroundColor: 0xFFFFFF }} >
       <Fragment>
@@ -40,6 +45,30 @@ export default ({ values, ...props }) => {
           <TilingSprite texture={hair} height={64} width={64} scale={2} position={[34, 0]} tilePosition={[0, 192]} filters={[hairShader]} />
           <TilingSprite texture={hair} height={64} width={64} scale={2} position={[107, 0]} tilePosition={[0, 0]} filters={[hairShader]} />
           <TilingSprite texture={hair} height={64} width={64} scale={2} position={[175, 0]} tilePosition={[0, 128]} filters={[hairShader]} />
+        </Fragment>
+      )}
+      {shirt && (
+        <Fragment>
+          <TilingSprite texture={shirt} height={64} width={64} scale={2} position={[-32, 0]} tilePosition={[0, 256]} />
+          <TilingSprite texture={shirt} height={64} width={64} scale={2} position={[34, 0]} tilePosition={[0, 192]} />
+          <TilingSprite texture={shirt} height={64} width={64} scale={2} position={[107, 0]} tilePosition={[0, 0]} />
+          <TilingSprite texture={shirt} height={64} width={64} scale={2} position={[175, 0]} tilePosition={[0, 128]} />
+        </Fragment>
+      )}
+      {legs && (
+        <Fragment>
+          <TilingSprite texture={legs} height={64} width={64} scale={2} position={[-32, 0]} tilePosition={[0, 256]} />
+          <TilingSprite texture={legs} height={64} width={64} scale={2} position={[34, 0]} tilePosition={[0, 192]} />
+          <TilingSprite texture={legs} height={64} width={64} scale={2} position={[107, 0]} tilePosition={[0, 0]} />
+          <TilingSprite texture={legs} height={64} width={64} scale={2} position={[175, 0]} tilePosition={[0, 128]} />
+        </Fragment>
+      )}
+      {back && (
+        <Fragment>
+          <TilingSprite texture={back} height={64} width={64} scale={2} position={[-32, 0]} tilePosition={[0, 256]} />
+          <TilingSprite texture={back} height={64} width={64} scale={2} position={[34, 0]} tilePosition={[0, 192]} />
+          <TilingSprite texture={back} height={64} width={64} scale={2} position={[107, 0]} tilePosition={[0, 0]} />
+          <TilingSprite texture={back} height={64} width={64} scale={2} position={[175, 0]} tilePosition={[0, 128]} />
         </Fragment>
       )}
     </Stage>
