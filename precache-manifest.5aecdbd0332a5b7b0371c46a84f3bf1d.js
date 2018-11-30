dd0efb1db3176c33bf7be598fc1165f707e8c9e1
David Konsumer <konsumer@jetboystudio.com>
2018-11-30 01:58:33 -0800

    gh-pages update

---
+++ precache-manifest.5aecdbd0332a5b7b0371c46a84f3bf1d.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b75c09b01981b192b65e71c63ee0a2f",
+    "url": "/universal-sprites/static/media/wings_no_th-sh.8b75c09b.png"
+  },
+  {
+    "revision": "11470afb02b1a4362fbc",
+    "url": "/universal-sprites/static/css/main.af4ddee5.chunk.css"
+  },
+  {
+    "revision": "76465affb15cec6c3c71",
+    "url": "/universal-sprites/static/js/1.76465aff.chunk.js"
+  },
+  {
+    "revision": "84805b43944eebd046ef",
+    "url": "/universal-sprites/static/js/runtime~main.84805b43.js"
+  },
+  {
+    "revision": "b0b61e12e19126eb42a0ababef7ac074",
+    "url": "/universal-sprites/static/media/human.b0b61e12.png"
+  },
+  {
+    "revision": "4da80e3578fee457d8b613e9e33efd4c",
+    "url": "/universal-sprites/static/media/orc.4da80e35.png"
+  },
+  {
+    "revision": "d547cf07ae69ba109a83e4d34f77b443",
+    "url": "/universal-sprites/static/media/human.d547cf07.png"
+  },
+  {
+    "revision": "a982fe3b173d4d463dc9c5fe9980cb52",
+    "url": "/universal-sprites/static/media/orc.a982fe3b.png"
+  },
+  {
+    "revision": "38e51e9495a0c7b5c8d7e73ee8fbfc78",
+    "url": "/universal-sprites/static/media/skeleton.38e51e94.png"
+  },
+  {
+    "revision": "af41a0bc5a3a444f7d1c3a95d3bf9aa7",
+    "url": "/universal-sprites/static/media/bangslong2.af41a0bc.png"
+  },
+  {
+    "revision": "d6534a18307d15682961ab92ca147e44",
+    "url": "/universal-sprites/static/media/bunches.d6534a18.png"
+  },
+  {
+    "revision": "2d9dd399e44ccdf79a8b296190fcdaf5",
+    "url": "/universal-sprites/static/media/long.2d9dd399.png"
+  },
+  {
+    "revision": "db8be3d6126855426581d9980145c897",
+    "url": "/universal-sprites/static/media/ponytail2.db8be3d6.png"
+  },
+  {
+    "revision": "1d5757edc087387a93ad226604f6c418",
+    "url": "/universal-sprites/static/media/princess.1d5757ed.png"
+  },
+  {
+    "revision": "c062dfc43d657b7b6c156bd44952bb5a",
+    "url": "/universal-sprites/static/media/shoulderl.c062dfc4.png"
+  },
+  {
+    "revision": "027baed61d69ab88a19fa0d49c3b822c",
+    "url": "/universal-sprites/static/media/shoulderr.027baed6.png"
+  },
+  {
+    "revision": "6f0e76c888b2b52187c504208e24b40f",
+    "url": "/universal-sprites/static/media/unkempt.6f0e76c8.png"
+  },
+  {
+    "revision": "49ab73e73007cc64030657bdf04e9c0f",
+    "url": "/universal-sprites/static/media/xlong.49ab73e7.png"
+  },
+  {
+    "revision": "a58116d99a12d6f18547ec7ae985b900",
+    "url": "/universal-sprites/static/media/xlongknot.a58116d9.png"
+  },
+  {
+    "revision": "4aabeb5770ec958784175e85af2784c9",
+    "url": "/universal-sprites/static/media/bangslong2.4aabeb57.png"
+  },
+  {
+    "revision": "53d0b18767b942fbe7a25bfbb03a0aca",
+    "url": "/universal-sprites/static/media/bunches.53d0b187.png"
+  },
+  {
+    "revision": "eab41937e3fce9d229687006e63d4602",
+    "url": "/universal-sprites/static/media/long.eab41937.png"
+  },
+  {
+    "revision": "5570ca1db7f42bf2977ac1d5a7124f41",
+    "url": "/universal-sprites/static/media/ponytail2.5570ca1d.png"
+  },
+  {
+    "revision": "189dfbe4d7922b6158508f3a662c452b",
+    "url": "/universal-sprites/static/media/princess.189dfbe4.png"
+  },
+  {
+    "revision": "2f582cab8705bcb5fab648fb81e513d0",
+    "url": "/universal-sprites/static/media/shoulderl.2f582cab.png"
+  },
+  {
+    "revision": "d171cc20b6c97143c770c51fc3c89820",
+    "url": "/universal-sprites/static/media/shoulderr.d171cc20.png"
+  },
+  {
+    "revision": "f97eed6bfab7a340b8365d2c2210b217",
+    "url": "/universal-sprites/static/media/xlong.f97eed6b.png"
+  },
+  {
+    "revision": "f97d9646482c93f7a1c6e0d05c259c01",
+    "url": "/universal-sprites/static/media/xlongknot.f97d9646.png"
+  },
+  {
+    "revision": "4fc239f6a275eee834cf91c1e4000348",
+    "url": "/universal-sprites/static/media/tattercape_black.4fc239f6.png"
+  },
+  {
+    "revision": "5ce36c257802bc00ca002a1664a0dbbd",
+    "url": "/universal-sprites/static/media/tattercape_blue.5ce36c25.png"
+  },
+  {
+    "revision": "96e3889eb1e40783417ba0d79184a9b6",
+    "url": "/universal-sprites/static/media/tattercape_brown.96e3889e.png"
+  },
+  {
+    "revision": "c825a1a25b172fc74ed18a7b86af3f1b",
+    "url": "/universal-sprites/static/media/tattercape_gray.c825a1a2.png"
+  },
+  {
+    "revision": "2191f5e95aec6a168f9fc21e5d65cac1",
+    "url": "/universal-sprites/static/media/tattercape_green.2191f5e9.png"
+  },
+  {
+    "revision": "10f524e0a5fdf6a494ec670ddd56916e",
+    "url": "/universal-sprites/static/media/tattercape_lavender.10f524e0.png"
+  },
+  {
+    "revision": "1422eb91e247616dcf3bafe3ed72042c",
+    "url": "/universal-sprites/static/media/tattercape_maroon.1422eb91.png"
+  },
+  {
+    "revision": "e0dd100564c4211251f2d343569d0345",
+    "url": "/universal-sprites/static/media/tattercape_pink.e0dd1005.png"
+  },
+  {
+    "revision": "ef9f37399e6fe4bbff367bf3a4c1cd7c",
+    "url": "/universal-sprites/static/media/tattercape_red.ef9f3739.png"
+  },
+  {
+    "revision": "af276b3b955a02d2c3fa83f6ffa75dea",
+    "url": "/universal-sprites/static/media/tattercape_white.af276b3b.png"
+  },
+  {
+    "revision": "633abbf16883af0d043ec5cf7c588b91",
+    "url": "/universal-sprites/static/media/tattercape_yellow.633abbf1.png"
+  },
+  {
+    "revision": "a3d8ab1c5fb3b9d849164da0e5f81474",
+    "url": "/universal-sprites/static/media/leather_cap_female.a3d8ab1c.png"
+  },
+  {
+    "revision": "439eaf2cee1c7a8e4204767c7662bbe3",
+    "url": "/universal-sprites/static/media/leather_cap_male.439eaf2c.png"
+  },
+  {
+    "revision": "c4b616d7efb0a721e5ff6b00b3db2551",
+    "url": "/universal-sprites/static/media/golden_helm_female.c4b616d7.png"
+  },
+  {
+    "revision": "10f11bbb56f07040447273a577f2bc72",
+    "url": "/universal-sprites/static/media/metal_helm_female.10f11bbb.png"
+  },
+  {
+    "revision": "4db069ab68688d3c4d2c16cf9312bdd0",
+    "url": "/universal-sprites/static/media/golden_helm_male.4db069ab.png"
+  },
+  {
+    "revision": "4ebccc5ace7b4a32163d8e8d197f6cf2",
+    "url": "/universal-sprites/static/media/metal_helm_male.4ebccc5a.png"
+  },
+  {
+    "revision": "a0845fc365b1e4844b8ad6c927db9d42",
+    "url": "/universal-sprites/static/media/chain_hood_female.a0845fc3.png"
+  },
+  {
+    "revision": "5520daf6a83981a711d8e66bc28d081b",
+    "url": "/universal-sprites/static/media/cloth_hood_female.5520daf6.png"
+  },
+  {
+    "revision": "9657ccc2e411b17e5e905418dd383702",
+    "url": "/universal-sprites/static/media/chain_hood_male.9657ccc2.png"
+  },
+  {
+    "revision": "1c18294dcebad0556011d4fea311bbf1",
+    "url": "/universal-sprites/static/media/cloth_hood_male.1c18294d.png"
+  },
+  {
+    "revision": "11470afb02b1a4362fbc",
+    "url": "/universal-sprites/static/js/main.11470afb.chunk.js"
+  },
+  {
+    "revision": "8e8386e504673ad0a7ce9755e2437a18",
+    "url": "/universal-sprites/static/media/mail_female.8e8386e5.png"
+  },
+  {
+    "revision": "cbc75a366fc5f1429617d409693eb39c",
+    "url": "/universal-sprites/static/media/mail_male.cbc75a36.png"
+  },
+  {
+    "revision": "e000864805bdcafaa43d4e64bd615bc4",
+    "url": "/universal-sprites/static/media/jacket_female.e0008648.png"
+  },
+  {
+    "revision": "4e9f1667c7c9fe354827246c1a0ba7be",
+    "url": "/universal-sprites/static/media/jacket_male.4e9f1667.png"
+  },
+  {
+    "revision": "96070209e8416a3a21d8b7a11dae0a32",
+    "url": "/universal-sprites/static/media/dress_w_sash_female.96070209.png"
+  },
+  {
+    "revision": "57a054879e17136741a4a9a32f910cbd",
+    "url": "/universal-sprites/static/media/tightdress_black.57a05487.png"
+  },
+  {
+    "revision": "2e9929dd5a807507e5dfa1726bc6e5e7",
+    "url": "/universal-sprites/static/media/tightdress_lightblue.2e9929dd.png"
+  },
+  {
+    "revision": "beba64ddeccaba48d309a82c870d3c76",
+    "url": "/universal-sprites/static/media/tightdress_red.beba64dd.png"
+  },
+  {
+    "revision": "d5fae66c7c55d15da3b038b8beecf5fa",
+    "url": "/universal-sprites/static/media/tightdress_white.d5fae66c.png"
+  },
+  {
+    "revision": "7bf82209dff8d1eb20616039e16c2cdc",
+    "url": "/universal-sprites/static/media/underdress.7bf82209.png"
+  },
+  {
+    "revision": "53919ac86d43d6c827d9f84f3c79142d",
+    "url": "/universal-sprites/static/media/arms_female.53919ac8.png"
+  },
+  {
+    "revision": "47a8d03b43ccf421c030e6dc722469f0",
+    "url": "/universal-sprites/static/media/arms_male.47a8d03b.png"
+  },
+  {
+    "revision": "ea033733630a8ca73db6725ca91dd2a0",
+    "url": "/universal-sprites/static/media/chest_female.ea033733.png"
+  },
+  {
+    "revision": "89cd3ba1a8e597efcda7671b7f0eaf88",
+    "url": "/universal-sprites/static/media/chest_male.89cd3ba1.png"
+  },
+  {
+    "revision": "830e46f1e0b47cc05927af209d2cbec9",
+    "url": "/universal-sprites/static/media/chest_female.830e46f1.png"
+  },
+  {
+    "revision": "ca5fb15c1b4752c8c70fb9ea898c1fea",
+    "url": "/universal-sprites/static/media/chest_male.ca5fb15c.png"
+  },
+  {
+    "revision": "a426af205bfca3c985f7da31e149f7af",
+    "url": "/universal-sprites/static/media/arms_female.a426af20.png"
+  },
+  {
+    "revision": "fe634bbf320e18f00954340601603f39",
+    "url": "/universal-sprites/static/media/arms_male.fe634bbf.png"
+  },
+  {
+    "revision": "d99f65d828ff590cb3015a5603bc4d79",
+    "url": "/universal-sprites/static/media/chest_female.d99f65d8.png"
+  },
+  {
+    "revision": "b3ccf7a9f58c44f3de1db5a9896213a0",
+    "url": "/universal-sprites/static/media/chest_male.b3ccf7a9.png"
+  },
+  {
+    "revision": "8a8bcb6bf194507dade79eae0357a744",
+    "url": "/universal-sprites/static/media/blue.8a8bcb6b.png"
+  },
+  {
+    "revision": "e61547320ee2a88f9a74d423a4a3caf5",
+    "url": "/universal-sprites/static/media/brown.e6154732.png"
+  },
+  {
+    "revision": "10ca68ba5c4bc3c4e735b5bb9e44712e",
+    "url": "/universal-sprites/static/media/dark brown.10ca68ba.png"
+  },
+  {
+    "revision": "2803b29d2ef9f3bcbde1b190f44f415d",
+    "url": "/universal-sprites/static/media/dark gray.2803b29d.png"
+  },
+  {
+    "revision": "da0534a8e2dc20a7b66d284dc7719f67",
+    "url": "/universal-sprites/static/media/forest green.da0534a8.png"
+  },
+  {
+    "revision": "1f1f7dfda8bbad3622932e6089699183",
+    "url": "/universal-sprites/static/media/light gray.1f1f7dfd.png"
+  },
+  {
+    "revision": "8619839f542baf43ee00bde3a314dac4",
+    "url": "/universal-sprites/static/media/purple.8619839f.png"
+  },
+  {
+    "revision": "47b7705ac81f532f997e7708b3a60db1",
+    "url": "/universal-sprites/static/media/red.47b7705a.png"
+  },
+  {
+    "revision": "3d9c9c58fe0153b3656abd0488a58f63",
+    "url": "/universal-sprites/static/media/white.3d9c9c58.png"
+  },
+  {
+    "revision": "dddb246e8f7450494295f4ad72cc6331",
+    "url": "/universal-sprites/static/media/brown_longsleeve.dddb246e.png"
+  },
+  {
+    "revision": "5c2ad9526f7a0bafa5b1f673a1ffd557",
+    "url": "/universal-sprites/static/media/maroon_longsleeve.5c2ad952.png"
+  },
+  {
+    "revision": "fae8e2b3d99ad4fbfaa62250e5f00f35",
+    "url": "/universal-sprites/static/media/teal_longsleeve.fae8e2b3.png"
+  },
+  {
+    "revision": "cfc8613d2449da061e9d538e603276b5",
+    "url": "/universal-sprites/static/media/white_longsleeve.cfc8613d.png"
+  },
+  {
+    "revision": "81de49e8ecb85c65734a55a95f6117dc",
+    "url": "/universal-sprites/static/media/shield_male_cutoutforbody.81de49e8.png"
+  },
+  {
+    "revision": "e2f7e052b2f1d15c2e6efb8be1d852bb",
+    "url": "/universal-sprites/static/media/shield_male_cutoutforhat.e2f7e052.png"
+  },
+  {
+    "revision": "5e0387f1a421289a4248de7c3f21f52a",
+    "url": "/universal-sprites/static/media/longsword_female.5e0387f1.png"
+  },
+  {
+    "revision": "71921a117d9b48575ecbbeac96173aa3",
+    "url": "/universal-sprites/static/media/rapier_female.71921a11.png"
+  },
+  {
+    "revision": "752b7c27d94a51a34649acac32271672",
+    "url": "/universal-sprites/static/media/longsword_male.752b7c27.png"
+  },
+  {
+    "revision": "a922e4885f7e0f7e75b16f5a6dae2fa2",
+    "url": "/universal-sprites/static/media/rapier_male.a922e488.png"
+  },
+  {
+    "revision": "7e9b06f3b699a07cec85bcd848106333",
+    "url": "/universal-sprites/static/media/dragonspear.7e9b06f3.png"
+  },
+  {
+    "revision": "e82d0b9b0b15dc7c6820e7b3b5a9245c",
+    "url": "/universal-sprites/static/media/spear.e82d0b9b.png"
+  },
+  {
+    "revision": "f3a1fb48de2a443d7250ed4450ac5d3d",
+    "url": "/universal-sprites/static/media/trident.f3a1fb48.png"
+  },
+  {
+    "revision": "1bca5b315b7f02a0a7d0cb93efd62a49",
+    "url": "/universal-sprites/static/media/bow.1bca5b31.png"
+  },
+  {
+    "revision": "717e809521da813c8f6723d154d045df",
+    "url": "/universal-sprites/static/media/bow_skeleton.717e8095.png"
+  },
+  {
+    "revision": "f9fea2b9e29f7317fc9d88c8a8295bd7",
+    "url": "/universal-sprites/static/media/greatbow.f9fea2b9.png"
+  },
+  {
+    "revision": "c490dc1c3f05c87efe3af3f607377849",
+    "url": "/universal-sprites/static/media/recurvebow.c490dc1c.png"
+  },
+  {
+    "revision": "6f7efc3d5d4e2703e7117b8b764c5a4c",
+    "url": "/universal-sprites/static/media/spear_female.6f7efc3d.png"
+  },
+  {
+    "revision": "33acb14258e9aa71d564a8d58a5c3eec",
+    "url": "/universal-sprites/static/media/spear_male.33acb142.png"
+  },
+  {
+    "revision": "3f2f64f08099225e82f8330d89cbd4c4",
+    "url": "/universal-sprites/index.html"
+  }
+];
